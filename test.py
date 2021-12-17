de7b9a4ff40eeda4ae6465c089792dab397e29c3
iljamaurer <ilja.maurer@finealyze.com>
2021-12-17 21:26:17 +0100

    new example

--- test.py
+++
@@ -1,6 +0,0 @@
-from sktime_dev.sktime.transformations.series.exponent import ExponentTransformer
-from sktime_dev.sktime.transformations.series.series_augmenter import (
-    WhiteNoiseSeriesAugmenter,
-)
-
-WhiteNoiseSeriesAugmenter()
